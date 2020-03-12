cbed2c933710b15aec6a58bfba349d9c60ad8d2c
Begzee <begzsuren0430@gmail.com>
2020-03-12 08:01:18 -0700

    roll dice btn iin buren ajilgaatai bolgono

--- app.js
+++ app.js
@@ -27,10 +27,60 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDom.src = "dice-" + diceNumber + ".png";
   //   зургыг харуулах
   diceDom.style.display = "block";
+
   // буусан тоо 1 ээс ялгаатай бол идэвтэй тоглогчын оноог нэмнэ
   if (diceNumber != 1) {
     // шооны буусан тоо 1 ээс ялгаатай байна
+
+    roundScore += diceNumber;
+
+    //delgetseniin currentiig haruulah
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
     // шоо 1 буусан байна
+
+    // тоглогчын оноог 0 болгох
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    roundScore = 0;
+    //тоглогчын ээлжийг солино
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //идэвхийн тэмдгийг солих
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    //шоог түр алга болгох
+    diceDom.style.display = "none";
   }
+});
+
+// hold button ii event
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  //энэ эвэнтээр буусан оноог нийт оноо руу нэмнэ
+  score[activePlayer] += roundScore;
+  // дэлгэцэнд оноог харуулах
+  document.getElementById("score-" + activePlayer).textContent =
+    score[activePlayer];
+  // шооны тоог тэглэх
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+
+  // ялагчийг тодруулах хэсэг
+  score[activePlayer] >=100?()
+});
+
+// new game ийн эвэнт
+
+document.querySelector(".btn-new").addEventListener("click", function() {
+  // шоог алга болгох
+  diceDom.style.display = "none";
+  // хоёр тоглогчийн оноог тэглэх
+  score[0] = 0;
+  score[1] = 0;
+
+  // дэлгэцийн бүх утгыг тэглэх
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
 });
